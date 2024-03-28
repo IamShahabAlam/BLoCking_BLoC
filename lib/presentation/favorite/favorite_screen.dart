3c52c27c4177da8b4d2e58fbcd66d7dc2308c5e8
Shahab Alam <iamShahabAlam@gmail.com>
2024-03-29 02:13:36 +0500

    fav ui

--- lib/presentation/favorite/favorite_screen.dart
+++ lib/presentation/favorite/favorite_screen.dart
@@ -22,34 +22,61 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Favoite Screen')),
+      appBar: AppBar(
+        title: const Text('Favoite Screen'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          BlocBuilder<FavoriteBloc, FavoriteState>(builder: (context, state) {
+            return Visibility(
+                visible: state.selectedList.isNotEmpty,
+                child: IconButton(
+                    onPressed: () {
+                      context.read<FavoriteBloc>().add(DeleteSelectedItems());
+                    },
+                    icon: const Icon(Icons.delete_outlined)));
+          })
+        ],
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Expanded(child: BlocBuilder<FavoriteBloc, FavoriteState>(builder: (_, state) {
-            return ListView.builder(
-              itemCount: state.favoriteList.length,
-              itemBuilder: (context, i) {
-                return ListTile(
-                  title: Text(state.favoriteList[i].name),
-                  leading: Checkbox(
-                      value: state.favoriteList[i].isSelected,
-                      onChanged: (val) {
-                        // context.read<FavoriteBloc>().add();
-                      }),
-                  trailing: IconButton(
-                      onPressed: () {
-                        // context.read<FavoriteBloc>().add();
-                      },
-                      icon: Icon(
-                        state.favoriteList[i].isFavorite ? Icons.favorite : Icons.favorite_border,
-                        color: state.favoriteList[i].isFavorite ? Colors.red : Colors.grey,
-                      )),
-                );
-              },
-            );
-          }))
+          Expanded(
+              child: BlocBuilder<FavoriteBloc, FavoriteState>(
+                  buildWhen: (p, c) => p.favoriteList != c.favoriteList,
+                  builder: (_, state) {
+                    switch (state.listStatus) {
+                      case ListStatus.loading:
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      case ListStatus.failure:
+                        return const Center(
+                          child: Text('Something went Wrong!'),
+                        );
+                      default:
+                        return ListView.builder(
+                          itemCount: state.favoriteList.length,
+                          itemBuilder: (context, i) {
+                            return ListTile(
+                                title: Text(state.favoriteList[i].name),
+                                leading: Checkbox(
+                                    value: state.favoriteList[i].isSelected,
+                                    onChanged: (val) {
+                                      context.read<FavoriteBloc>().add(MarkSelect(index: i));
+                                    }),
+                                trailing: IconButton(
+                                    onPressed: () {
+                                      context.read<FavoriteBloc>().add(MarkFavorite(index: i));
+                                    },
+                                    icon: Icon(
+                                      state.favoriteList[i].isFavorite ? Icons.favorite : Icons.favorite_border,
+                                      color: state.favoriteList[i].isFavorite ? Colors.red : Colors.grey,
+                                    )));
+                          },
+                        );
+                    }
+                  }))
         ],
       ),
     );
