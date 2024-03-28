87d7c3938b8523ac23ded89421d245e658d537ed
Shahab Alam <iamShahabAlam@gmail.com>
2024-03-29 02:13:12 +0500

    fav model updated

--- lib/model/favorite_model.dart
+++ lib/model/favorite_model.dart
@@ -1,12 +1,19 @@
-class FavoriteModel {
+// ignore_for_file: must_be_immutable
+
+import 'package:equatable/equatable.dart';
+
+class FavoriteModel extends Equatable {
   final String name;
   final int id;
-  final bool isFavorite, isSelected;
+  bool isFavorite, isSelected;
 
-  const FavoriteModel({required this.name, required this.id, this.isFavorite = false, this.isSelected = false});
+  FavoriteModel({required this.name, required this.id, this.isFavorite = false, this.isSelected = false});
 
   FavoriteModel copyWith({String? name, int? id, bool? isFavorite, bool? isSelected}) {
     return FavoriteModel(
         name: name ?? this.name, id: id ?? this.id, isFavorite: isFavorite ?? this.isFavorite, isSelected: isSelected ?? this.isSelected);
   }
+
+  @override
+  List<Object?> get props => [id, name, isFavorite, isSelected];
 }
