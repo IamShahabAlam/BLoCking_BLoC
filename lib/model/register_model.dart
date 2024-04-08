787d42808cb14475d6aa960f203d1f976777c875
Shahab Alam <iamShahabAlam@gmail.com>
2024-04-08 22:27:31 +0500

    reg model

--- lib/model/register_model.dart
+++ lib/model/register_model.dart
@@ -6,7 +6,7 @@ class RegisterModel extends Equatable {
 
   const RegisterModel({this.name = '', this.email = '', this.password = '', this.city = '', this.age = 0});
 
-  RegisterModel copyWith(String? name, String? email, String? password, String? city, num? age) {
+  RegisterModel copyWith({String? name, String? email, String? password, String? city, num? age}) {
     return RegisterModel(
         name: name ?? this.name, email: email ?? this.email, password: password ?? this.password, city: city ?? this.city, age: age ?? this.age);
   }
