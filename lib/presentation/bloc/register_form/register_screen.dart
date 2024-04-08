c6dc415c7b80944f6da757390ded604712b9305d
Shahab Alam <iamShahabAlam@gmail.com>
2024-04-08 22:27:46 +0500

    reg ui

--- lib/presentation/bloc/register_form/register_screen.dart
+++ lib/presentation/bloc/register_form/register_screen.dart
@@ -1,5 +1,12 @@
+import 'package:bloc_app/bloc/register_form/register_bloc.dart';
+import 'package:bloc_app/model/register_model.dart';
+import 'package:bloc_app/utils/configs/app_colors.dart';
+import 'package:bloc_app/utils/widgets/custom_btn.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../utils/widgets/custom_field.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -12,10 +19,67 @@ class RegisterScreen extends StatelessWidget {
     final emailController = TextEditingController();
     final passwordController = TextEditingController();
     return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(
         title: const Text('BloC Register Screen'),
       ),
-      // body: ,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Text(
+              'Register Form',
+              style: TextStyle(fontSize: 35.0, color: AppColors.gradient3, fontWeight: FontWeight.bold),
+            ),
+            // name ------------
+            CustomField(
+              controller: nameController,
+              hintText: 'Name',
+            ),
+            // AGe ------------
+            CustomField(
+              controller: ageController,
+              hintText: "Age",
+            ),
+            // City ------------
+            CustomField(
+              controller: cityController,
+              hintText: 'City',
+            ),
+            // EMail ------------
+            CustomField(
+              controller: emailController,
+              hintText: 'Email',
+            ),
+            // Password ------------
+            CustomField(
+              controller: passwordController,
+              hintText: 'Password',
+            ),
+
+            CustomButton(
+                text: 'Register',
+                onPressed: () {
+                  var regModel = RegisterModel(
+                      name: nameController.text.trim(),
+                      email: emailController.text.trim(),
+                      password: passwordController.text.trim(),
+                      city: cityController.text.trim(),
+                      age: ageController.text.isNotEmpty ? int.parse(ageController.text) : 0);
+
+                  // regModel.copyWith(
+                  //     name: nameController.text.trim(),
+                  //     email: emailController.text.trim(),
+                  //     password: passwordController.text.trim(),
+                  //     city: cityController.text.trim(),
+                  //     age: ageController.text.isNotEmpty ? int.parse(ageController.text) : 0);
+
+                  context.read<RegisterBloc>().add(OnRegister(regModel: regModel));
+                }),
+          ],
+        ),
+      ),
     );
   }
 }
