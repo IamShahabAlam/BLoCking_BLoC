416fbd765fc137e95bb03f78d4afac2b3de11a50
Shahab Alam <iamShahabAlam@gmail.com>
2024-04-15 23:33:35 +0500

    utils update

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -1,3 +1,6 @@
 class AppStrings {
-  static String productEndpoint = 'https://fakestoreapi.com/products';
+  static const String productEndpoint = 'https://fakestoreapi.com/products';
+  static const String weatherEndpoint = 'https://api.openweathermap.org/data/2.5/forecast';
+
+  static const String weatherApiKey = '4af409a4c67493e64a7c44c96d9c51e3';
 }
